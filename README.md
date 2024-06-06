86b659fd9349d1635e89a885a77ee09726dc2596
LiviaMedeiros <livia@cirno.name>
2024-06-07 03:00:00 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Puella Historia
 
-Collected 1043 records from 29 installations.
+Collected 1297 records from 30 installations.
 
 <p align="right"><img src="https://xn--80aalyho.xn--p1ai/magireco/NAgitan/img/kagome.png" /></p>
---
+++ magica/js/quest/scene0/parts/UseItemView.js
@@ -0,0 +1,2 @@
+define("underscore backbone backboneCommon ajaxControl command text!template/quest/scene0/UseItem.html text!css/quest/scene0/UseItem.css".split(" "),function(c,d,e,h,k,f,g){var b;return d.View.extend({events:function(){return{}},initialize:function(a){this.template=c.template(f);this.pageModel=a.pageModel;e.addStyle(g);b=this.createModel({pageModel:this.pageModel})},render:function(){this.$el.html(this.template({model:b}));return this},createModel:function(a){a=a.pageModel;return{type:a.type,itemNum:a.itemInfo.quantity}},
+removeView:function(){this.off();this.remove()}})});
