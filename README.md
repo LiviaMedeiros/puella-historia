bf647de3c669cf4783c2f42c192d93faa42d7950
Livia Medeiros <livia@cirno.name>
2024-06-06 06:09:10 +0000

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Puella Historia
+
+Collected 740 records from 28 installations.
+
+<p align="right"><img src="https://xn--80aalyho.xn--p1ai/magireco/NAgitan/img/kagome.png" /></p>
---
+++ magica/js/system/searchPathMapping.js
@@ -0,0 +1,1 @@
+define(["backboneCommon"],function(b){var a={},c={friend:"/friend_search/_search"},d="/search",e="";if(location.href.match("file://")||location.href.match("http://localhost:5963")||location.href.match("https://localhost:5963"))d="/magica/json",e=".json";b.searchLinkList=[];a.pathSet=function(){for(var a in c)b.searchLinkList[a]=d+c[a]+e};return a});
